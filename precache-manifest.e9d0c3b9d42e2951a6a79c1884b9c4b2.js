5a2cf387380b5439667df33814e539d36013462c
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-11 23:20:03 +0300

    Updates

---
+++ precache-manifest.e9d0c3b9d42e2951a6a79c1884b9c4b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75da891ca94aabd396a30dbd55da4679",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "33c57c8669d1c373b4e3",
+    "url": "/react_phone-catalog/static/css/main.d2bf7407.chunk.css"
+  },
+  {
+    "revision": "711ce61865fd8a5e53b0",
+    "url": "/react_phone-catalog/static/js/2.309ee28b.chunk.js"
+  },
+  {
+    "revision": "33c57c8669d1c373b4e3",
+    "url": "/react_phone-catalog/static/js/main.e054ca5c.chunk.js"
+  },
+  {
+    "revision": "9b3e1d68b9cc1dd9a925",
+    "url": "/react_phone-catalog/static/js/runtime~main.25bc40f7.js"
+  }
+]);
